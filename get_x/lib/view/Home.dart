78266d890eff2e7a1a4aeeb20ad6e32923af1113
broccolism <mile3880@gmail.com>
2021-06-30 23:56:13 +0900

    :sparkle: HomePage

---
+++ get_x/lib/view/Home.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_x/state-management/controller/CounterController.dart';
+import 'package:get_x/state-management/view/Next.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(_) {
+    final Controller controller = Get.put(Controller());
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Learning GetX")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Page 1"),
+            GetX<Controller>(
+              builder: (_) {
+                return Text(
+                  'Count 1 : ${_.count1.value}',
+                  style: TextStyle(fontSize: 30),
+                );
+              },
+            ),
+            Obx(() {
+              return Text(
+                'Count 2 : ${controller.count2.value}',
+                style: TextStyle(fontSize: 30),
+              );
+            }),
+            Obx(() {
+              return Text(
+                'SUM : ${controller.sum}',
+                style: TextStyle(fontSize: 30),
+              );
+            }),
+            ElevatedButton(
+              onPressed: () {
+                controller.incrementTop();
+              },
+              child: Text(
+                'count 1 증가',
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                controller.incrementBottom();
+              },
+              child: Text(
+                'count 2 증가',
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.navigate_next),
+          onPressed: () {
+            Get.to(NextPage());
+          }),
+    );
+  }
+}
